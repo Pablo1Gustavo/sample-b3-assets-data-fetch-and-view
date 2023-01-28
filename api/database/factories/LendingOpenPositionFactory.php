dd84bf773a3fa526bc2a0e3f333b2fbc80353bac
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-28 20:37:14 -0300

    FT: add LendingOpenPosition model, factory and update seeder with it.

---
+++ api/database/factories/LendingOpenPositionFactory.php
@@ -0,0 +1,19 @@
+<?php
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LendingOpenPositionFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'isin' => fake()->regexify('[A-Z]{9}[0-9]{3}'),
+            'date' => fake()->dateTimeBetween('-2 months', '-2 days')->format('Y-m-d'),
+            'ticker_symbol_id' => \App\Models\TickerSymbol::inRandomOrder()->first()->id,
+            'balance_amount' => fake()->numberBetween(1, 10**8),
+            'average_price' => fake()->randomFloat(6, 1, 10000),
+            'total_balance' => fake()->randomFloat(2, 1, 10**9),
+        ];
+    }
+}
--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -8,5 +8,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         \App\Models\TickerSymbol::factory(20)->create();
+        \App\Models\LendingOpenPosition::factory(40)->create();
     }
 }
