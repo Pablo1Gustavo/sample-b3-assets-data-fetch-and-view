8232b29fdda64fdec683c6fab4e61d0b27651468
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-28 01:54:48 -0300

    TT: add the TickerSymbols factory.

---
+++ api/database/factories/TickerSymbolsFactory.php
@@ -0,0 +1,14 @@
+<?php
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TickerSymbolsFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'ticker_symbol' => fake()->regexify('[A-Z]{4}[0-9]{2}')
+        ];
+    }
+}
