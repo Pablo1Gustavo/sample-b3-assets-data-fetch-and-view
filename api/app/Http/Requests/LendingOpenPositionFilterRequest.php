8b7b13db6704f43edac2a46ecd502c8c080060ed
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-29 16:10:05 -0300

    RF: apply more appropriate rules to LendingOpenPositionFilterRequest.

--- api/app/Http/Requests/LendingOpenPositionFilterRequest.php
+++ api/app/Http/Requests/LendingOpenPositionFilterRequest.php
@@ -9,8 +9,8 @@ class LendingOpenPositionFilterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'start_date' => ['date', 'before:end_date', 'before:tomorrow'],
-            'end_date' => ['date', 'after:start_date', 'before:tomorrow'],
+            'start_date' => ['date', 'before_or_equal:end_date', 'before_or_equal:today'],
+            'end_date' => ['date', 'after_or_equal:start_date', 'before_or_equal:today'],
             'asset_id' => ['numeric']
         ];
     }
