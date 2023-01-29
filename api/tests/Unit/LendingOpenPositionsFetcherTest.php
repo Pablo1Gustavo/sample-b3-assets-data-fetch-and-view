13351ecf3d57fa1b2a5fb73ca4ac73bf6b1d6397
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-29 19:19:56 -0300

    TT: add some tests.

--- api/tests/Unit/LendingOpenPositionsFetcherTest.php
+++ api/tests/Unit/LendingOpenPositionsFetcherTest.php
@@ -7,36 +7,52 @@ use App\Jobs\LendingOpenPositionsFetcher;
 class LendingOpenPositionsFetcherTest extends TestCase
 {
     private $job;
-    private $previousDate;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->previousDate = date('Y-m-d',strtotime("-2 days"));
-        $this->job = new LendingOpenPositionsFetcher();
+        $previousDate = date('Y-m-d',strtotime("-3 days"));
+        $this->job = new LendingOpenPositionsFetcher($previousDate);
     }
 
     function test_get_fetch_url()
     {
-        $date = $this->previousDate;
-        $expectedUrl = 'https://arquivos.b3.com.br/api/download/requestname?fileName=LendingOpenPosition&date='.$date;
+        $previousDate = date('Y-m-d',strtotime("-3 days"));
+        $expectedUrl = 'https://arquivos.b3.com.br/api/download/requestname?fileName=LendingOpenPosition&date='.$previousDate;
 
-        $url = $this->job->getFetchUrl($date);
+        $url = $this->job->getFetchUrl();
 
         $this->assertSame($expectedUrl, $url);
     }
 
     function test_get_download_token()
     {
-        $token = $this->job->getDownloadToken($this->previousDate);
+        $token = $this->job->getDownloadToken();
 
         $this->assertTrue(base64_decode($token, true) !== false);
     }
 
+    public function test_set_date()
+    {
+        $this->job->setDate('1970-01-01');
+
+        $this->assertEquals($this->job->getDate(), '1970-01-01');
+    }
+
+    function test_get_download_token_for_tomorrow()
+    {
+        $tomorrowDate = date('Y-m-d',strtotime("+1 day"));
+        $this->job->setDate($tomorrowDate);
+
+        $token = $this->job->getDownloadToken();
+
+        $this->assertNull($token);
+    }
+
     function test_get_data()
     {
-        $token = $this->job->getDownloadToken($this->previousDate);
+        $token = $this->job->getDownloadToken();
 
         $data = $this->job->getData($token);
 
@@ -47,7 +63,7 @@ class LendingOpenPositionsFetcherTest extends TestCase
 
     function test_get_data_with_custom_headers()
     {
-        $token = $this->job->getDownloadToken($this->previousDate);
+        $token = $this->job->getDownloadToken();
 
         $customHeaders = ['date', 'ticker_symbol', 'isin', 'ticker_symbol_abrv', 'balance_amount', 'average_price', 'price_factor', 'total_balance'];
 
