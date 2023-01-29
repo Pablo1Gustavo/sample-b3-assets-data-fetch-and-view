12a42e8875ae72ac3c48cc526787fd0b9b2e4222
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-29 02:18:01 -0300

    FT: add model relationships and options.

--- api/app/Models/LendingOpenPosition.php
+++ api/app/Models/LendingOpenPosition.php
@@ -3,10 +3,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Builder;
 
 class LendingOpenPosition extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    protected $hidden = ['id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('ticker_symbol', function (Builder $builder) {
+            $builder->with('ticker_symbol');
+        });
+    }
+
+    public function ticker_symbol(): BelongsTo
+    {
+        return $this->belongsTo(TickerSymbol::class);
+    }
 }
--- api/app/Models/TickerSymbol.php
+++ api/app/Models/TickerSymbol.php
@@ -3,10 +3,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TickerSymbol extends Model
 {
     use HasFactory;
 
     public $timestamps = false;
+
+    public function lending_open_positions(): HasMany
+    {
+        return $this->hasMany(LendingOpenPosition::class);
+    }
 }
