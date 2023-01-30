6872e9c9d3f40f8dbdc8ea3c6abf0a5e02cf4767
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-29 23:20:46 -0300

    FT: add AssetSelector component.

---
+++ front/components/filter/AssetSelector.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-select
+        outlined
+        hide-details
+        label="Asset"
+        :items='assets'
+        v-model='asset'
+        item-text="name"
+        item-value="id"
+        @change="$emit('input', asset)"
+    />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: 'AssetSelector',
+
+    data: () => ({
+        asset: null,
+        assets: [
+            { id: 1, name: "ABCD01" },
+            { id: 2, name: "ABCD02" },
+            { id: 3, name: "ABCD03" }
+        ]
+    })
+})
+</script>
