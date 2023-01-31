72f0867528d11b90b3d3374387fbf8a97c591c84
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-31 00:52:23 -0300

    FT: add system colors.

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
     ssr: false,
     head: {
@@ -50,14 +48,9 @@ export default {
             dark: true,
             themes: {
                 dark: {
-                    primary: colors.blue.darken2,
-                    accent: colors.grey.darken3,
-                    secondary: colors.amber.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3,
-                    "system-blue-darken": "#1F2937"
+                    "sblue-darken": "#1F2937",
+                    "sblue-darken-2": "#1D2632",
+                    "sblack": "#1F2937"
                 }
             }
         }
