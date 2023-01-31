3e81f118f20a3a664e5e171835195d2cfb4de84a
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-31 01:57:03 -0300

    FT: insert line chart component on home page (sure).

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -5,6 +5,15 @@
                 <asset-selector v-model="asset"/>
             </v-col>
         </v-row>
+
+        <v-row>
+            <v-col cols="12">
+                <date-line-chart
+                    :line-colors="['#06C889','#DE9D1E']"
+                    :fetcher="lendingOpenPositionsFetcher"
+                />
+            </v-col>
+        </v-row>
     </div>
 </template>
 
@@ -16,6 +25,33 @@ export default defineComponent({
 
     data: () => ({
         asset: null
-    })
+    }),
+
+    computed: {
+        lendingOpenPositionsFetcher(): object
+        {
+            return {
+                url: 'lendingopenposition',
+                params: [{
+                    key: "asset",
+                    value: this.asset,
+                    required: true
+                }],
+                categoriesKey: 'date',
+                seriesFetcher: [
+                    {
+                        name: 'Total balance',
+                        key: 'total_balance',
+                        formatter: 'currency_ptbr'
+                    },
+                    {
+                        name: 'Average balance',
+                        key: 'average_price',
+                        formatter: 'currency_ptbr'
+                    }
+                ]
+            }
+        }
+    }
 })
 </script>
