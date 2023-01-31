33cbdf07787482006fb8a7d64c7d7502616087ca
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-31 01:53:13 -0300

    FT: add line chart component and insert on home page.

---
+++ front/components/filter/DatePicker.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-menu
+        v-model="menuOpened"
+    >
+        <template #activator="{ on, attrs }">
+            <v-text-field
+                outlined
+                readonly
+                label="Start date"
+                v-model="date"
+                v-bind="attrs"
+                v-on="on"
+            />
+            <v-date-picker
+                v-model="date"
+                range
+            />
+        </template>
+    </v-menu>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: 'DatePicker',
+
+    data: () => ({
+        menuOpened: false,
+        date: null
+    })
+})
+</script>
