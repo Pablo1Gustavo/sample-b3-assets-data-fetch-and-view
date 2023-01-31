a6fceecf382e40e6a6bdb9f02fef028164c03215
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-31 02:10:11 -0300

    FX: LendingOpenPositionFilterRequest rules problem.

--- api/app/Http/Requests/LendingOpenPositionFilterRequest.php
+++ api/app/Http/Requests/LendingOpenPositionFilterRequest.php
@@ -9,7 +9,7 @@ class LendingOpenPositionFilterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'start_date' => ['date', 'before_or_equal:end_date', 'before_or_equal:today'],
+            'start_date' => ['date', 'before_or_equal:today'],
             'end_date' => ['date', 'after_or_equal:start_date', 'before_or_equal:today'],
             'asset_id' => ['numeric']
         ];
