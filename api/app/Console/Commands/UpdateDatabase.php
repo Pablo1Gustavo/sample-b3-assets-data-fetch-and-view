d583a614b3cfe2bf046ee57b42f358451194e334
Pablo Gustavo Fernandes Maia <pablo1gustavo.dev@gmail.com>
2023-01-31 03:32:43 -0300

    FT: small changes in UpdateDatabase command.

--- api/app/Console/Commands/UpdateDatabase.php
+++ api/app/Console/Commands/UpdateDatabase.php
@@ -6,19 +6,19 @@ use Illuminate\Console\Command;
 
 class UpdateDatabase extends Command
 {
-    protected $signature = 'update:database {days=30}';
+    protected $signature = 'update:database {days=0}';
 
-    protected $description = 'Update the ticker_symbols table and add unexisting lending_open_positions';
+    protected $description = 'Add non-existent lending_open_positions and update ticker_symbols from a number of days ago';
 
     public function handle()
     {
         $daysInterval = intval($this->argument('days'));
 
-        $bar = $this->output->createProgressBar($daysInterval);
+        $bar = $this->output->createProgressBar($daysInterval + 1);
 
         $bar->start();
 
-        $job = new \App\Jobs\LendingOpenPositionsFetcher(date('Y-m-d'));
+        $job = new \App\Jobs\LendingOpenPositionsFetcher('');
 
         for ($day = 0; $day <= $daysInterval; $day++)
         {
